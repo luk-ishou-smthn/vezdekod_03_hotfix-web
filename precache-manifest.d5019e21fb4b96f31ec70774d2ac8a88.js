1f889b4d6a86d1fb9c27b93f5238c3e7a9d39fc5
Tema Smirnov <git.tema@smirnov.one>
2020-09-10 12:08:55 +0300

    Github Pages release

---
+++ precache-manifest.d5019e21fb4b96f31ec70774d2ac8a88.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb062a09aeb48d39bbd2556d58112d3",
+    "url": "/vezdekod_03_hotfix-web/index.html"
+  },
+  {
+    "revision": "a50639a8273585581bf7",
+    "url": "/vezdekod_03_hotfix-web/static/css/main.52c11fd2.chunk.css"
+  },
+  {
+    "revision": "23014250487c75d54f87",
+    "url": "/vezdekod_03_hotfix-web/static/js/2.2361b3c0.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vezdekod_03_hotfix-web/static/js/2.2361b3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50639a8273585581bf7",
+    "url": "/vezdekod_03_hotfix-web/static/js/main.31b769b8.chunk.js"
+  },
+  {
+    "revision": "aadf8dd5762f9564e233",
+    "url": "/vezdekod_03_hotfix-web/static/js/runtime-main.c7e10820.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vezdekod_03_hotfix-web/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vezdekod_03_hotfix-web/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vezdekod_03_hotfix-web/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vezdekod_03_hotfix-web/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vezdekod_03_hotfix-web/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "dd480b0d953c2e73dd0c54d7617d391a",
+    "url": "/vezdekod_03_hotfix-web/static/media/check-symbol.dd480b0d.svg"
+  },
+  {
+    "revision": "3e408ef227a082feb9d3527437ef78c2",
+    "url": "/vezdekod_03_hotfix-web/static/media/edit.3e408ef2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vezdekod_03_hotfix-web/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vezdekod_03_hotfix-web/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "36053a5b28bf11d11c66adc05394d4d3",
+    "url": "/vezdekod_03_hotfix-web/static/media/refresh-button.36053a5b.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vezdekod_03_hotfix-web/static/media/sub.4eb05fdd.png"
+  }
+]);
